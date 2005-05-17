1707abe7178ecb615c26c00c218aa811ef6ee075
Michael Bayne <mdb@threerings.net>
2005-05-17 23:56:43 +0000

    Do our model conversion before hand and remove dependencies on all the converters.

--- src/java/com/threerings/bang/client/ModelCache.java
+++ src/java/com/threerings/bang/client/ModelCache.java
@@ -71,21 +71,7 @@ public class ModelCache
 
     protected CloneCreator loadModel (String name)
     {
-        Node model = null;
-        if (name.equals("steamgunman")) {
-            model = loadOBJModel("media/models/" + name + ".obj");
-//             model = loadMd2Model("media/models/" + name + ".md2");
-            model.setLocalScale(0.06f);
-        } else if (name.equals("dirigible")) {
-            model = loadMd2Model("media/models/" + name + ".md2");
-            model.setLocalScale(0.2f);
-        } else if (name.equals("artillery")) {
-            model = loadMd3Model("media/models/" + name + ".md3");
-            model.setLocalScale(0.06f);
-        } else {
-            model = load3DSModel("media/models/" + name + ".3ds");
-            model.setLocalScale(0.06f);
-        }
+        Node model = loadJmeModel("media/models/" + name + ".jme");
 
         if (model == null) {
             model = new Node("error");
@@ -98,28 +84,7 @@ public class ModelCache
             model.attachChild(box);
 
         } else {
-            if (name.equals("dirigible")) {
-                TextureState ts = _ctx.getRenderer().createTextureState();
-                ts.setEnabled(true);
-                ts.setTexture(
-                    TextureManager.loadTexture(
-                        getClass().getClassLoader().getResource(
-                            "rsrc/media/textures/" + name + ".jpg"),
-                        Texture.MM_LINEAR,
-                        Texture.FM_LINEAR));
-                model.setRenderState(ts);
-            } else if (name.equals("artillery")) {
-                TextureState ts = _ctx.getRenderer().createTextureState();
-                ts.setEnabled(true);
-                ts.setTexture(
-                    TextureManager.loadTexture(
-                        getClass().getClassLoader().getResource(
-                            "rsrc/media/textures/" + name + ".png"),
-                        Texture.MM_LINEAR,
-                        Texture.FM_LINEAR));
-                model.setRenderState(ts);
-            }
-
+            model.setLocalScale(0.06f);
             model.setLocalTranslation(
                 new Vector3f(TILE_SIZE/2, TILE_SIZE/2, 0f));
         }
@@ -137,45 +102,18 @@ public class ModelCache
         return cc;
     }
 
-    protected Node load3DSModel (String path)
+    protected Node loadJmeModel (String path)
     {
-        MaxToJme converter = new MaxToJme();
-
         try {
-            ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            InputStream min = _ctx.getResourceManager().getResource(path);
-            converter.convert(new BufferedInputStream(min), bout);
-
-            JmeBinaryReader jbr = new JmeBinaryReader();
-            jbr.setProperty("bound", "box");
-            return jbr.loadBinaryFormat(
-                new ByteArrayInputStream(bout.toByteArray()));
-
-        } catch (IOException ioe) {
-            log.log(Level.WARNING, "Error loading model '" + path + "'.", ioe);
-            return null;
-        }
-    }
-
-    protected Node loadOBJModel (String path)
-    {
-        ObjToJme converter = new ObjToJme();
-
-        try {
-            ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            URL murl = getClass().getClassLoader().getResource("rsrc/" + path);
-            converter.setProperty("mtllib", murl);
-
-            InputStream min = _ctx.getResourceManager().getResource(path);
-            converter.convert(new BufferedInputStream(min), bout);
-
             JmeBinaryReader jbr = new JmeBinaryReader();
             jbr.setProperty("bound", "box");
             URL turl =
                 getClass().getClassLoader().getResource("rsrc/media/textures/");
             jbr.setProperty("texurl", turl);
-            return jbr.loadBinaryFormat(
-                new ByteArrayInputStream(bout.toByteArray()));
+            InputStream in =
+                getClass().getClassLoader().getResourceAsStream("rsrc/" + path);
+            log.info("Loading " + path + " from " + in + ": " + in.available());
+            return jbr.loadBinaryFormat(new BufferedInputStream(in));
 
         } catch (IOException ioe) {
             log.log(Level.WARNING, "Error loading model '" + path + "'.", ioe);
@@ -183,52 +121,98 @@ public class ModelCache
         }
     }
 
-    protected Node loadMd2Model (String path)
-    {
-        Md2ToJme converter = new Md2ToJme();
+//     protected Node load3DSModel (String path)
+//     {
+//         MaxToJme converter = new MaxToJme();
 
-        try {
-            ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            InputStream min = _ctx.getResourceManager().getResource(path);
-            converter.convert(new BufferedInputStream(min), bout);
+//         try {
+//             ByteArrayOutputStream bout = new ByteArrayOutputStream();
+//             InputStream min = _ctx.getResourceManager().getResource(path);
+//             converter.convert(new BufferedInputStream(min), bout);
 
-            JmeBinaryReader jbr = new JmeBinaryReader();
-            jbr.setProperty("bound", "box");
-            return jbr.loadBinaryFormat(
-                new ByteArrayInputStream(bout.toByteArray()));
+//             JmeBinaryReader jbr = new JmeBinaryReader();
+//             jbr.setProperty("bound", "box");
+//             return jbr.loadBinaryFormat(
+//                 new ByteArrayInputStream(bout.toByteArray()));
 
-        } catch (IOException ioe) {
-            log.log(Level.WARNING, "Error loading model '" + path + "'.", ioe);
-            return null;
-        }
-    }
+//         } catch (IOException ioe) {
+//             log.log(Level.WARNING, "Error loading model '" + path + "'.", ioe);
+//             return null;
+//         }
+//     }
 
-    protected Node loadMd3Model (String path)
-    {
-        Md3ToJme converter = new Md3ToJme();
+//     protected Node loadOBJModel (String path)
+//     {
+//         ObjToJme converter = new ObjToJme();
 
-        try {
-            ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            InputStream min = _ctx.getResourceManager().getResource(path);
-            converter.convert(new BufferedInputStream(min), bout);
+//         try {
+//             ByteArrayOutputStream bout = new ByteArrayOutputStream();
+//             URL murl = getClass().getClassLoader().getResource("rsrc/" + path);
+//             converter.setProperty("mtllib", murl);
 
-            JmeBinaryReader jbr = new JmeBinaryReader();
-            jbr.setProperty("bound", "box");
-            Node model = jbr.loadBinaryFormat(
-                new ByteArrayInputStream(bout.toByteArray()));
-            Quaternion r1 = new Quaternion();
-            r1.fromAngleAxis(-FastMath.PI/2, new Vector3f(-1,0,0));
-            Quaternion r2 = new Quaternion();
-            r2.fromAngleAxis(FastMath.PI/2, new Vector3f(0,1,0));
-            r1.multLocal(r2);
-            model.setLocalRotation(r1);
-            return model;
+//             InputStream min = _ctx.getResourceManager().getResource(path);
+//             converter.convert(new BufferedInputStream(min), bout);
 
-        } catch (IOException ioe) {
-            log.log(Level.WARNING, "Error loading model '" + path + "'.", ioe);
-            return null;
-        }
-    }
+//             JmeBinaryReader jbr = new JmeBinaryReader();
+//             jbr.setProperty("bound", "box");
+//             URL turl =
+//                 getClass().getClassLoader().getResource("rsrc/media/textures/");
+//             jbr.setProperty("texurl", turl);
+//             return jbr.loadBinaryFormat(
+//                 new ByteArrayInputStream(bout.toByteArray()));
+
+//         } catch (IOException ioe) {
+//             log.log(Level.WARNING, "Error loading model '" + path + "'.", ioe);
+//             return null;
+//         }
+//     }
+
+//     protected Node loadMd2Model (String path)
+//     {
+//         Md2ToJme converter = new Md2ToJme();
+
+//         try {
+//             ByteArrayOutputStream bout = new ByteArrayOutputStream();
+//             InputStream min = _ctx.getResourceManager().getResource(path);
+//             converter.convert(new BufferedInputStream(min), bout);
+
+//             JmeBinaryReader jbr = new JmeBinaryReader();
+//             jbr.setProperty("bound", "box");
+//             return jbr.loadBinaryFormat(
+//                 new ByteArrayInputStream(bout.toByteArray()));
+
+//         } catch (IOException ioe) {
+//             log.log(Level.WARNING, "Error loading model '" + path + "'.", ioe);
+//             return null;
+//         }
+//     }
+
+//     protected Node loadMd3Model (String path)
+//     {
+//         Md3ToJme converter = new Md3ToJme();
+
+//         try {
+//             ByteArrayOutputStream bout = new ByteArrayOutputStream();
+//             InputStream min = _ctx.getResourceManager().getResource(path);
+//             converter.convert(new BufferedInputStream(min), bout);
+
+//             JmeBinaryReader jbr = new JmeBinaryReader();
+//             jbr.setProperty("bound", "box");
+//             Node model = jbr.loadBinaryFormat(
+//                 new ByteArrayInputStream(bout.toByteArray()));
+//             Quaternion r1 = new Quaternion();
+//             r1.fromAngleAxis(-FastMath.PI/2, new Vector3f(-1,0,0));
+//             Quaternion r2 = new Quaternion();
+//             r2.fromAngleAxis(FastMath.PI/2, new Vector3f(0,1,0));
+//             r1.multLocal(r2);
+//             model.setLocalRotation(r1);
+//             return model;
+
+//         } catch (IOException ioe) {
+//             log.log(Level.WARNING, "Error loading model '" + path + "'.", ioe);
+//             return null;
+//         }
+//     }
 
     protected void dump (Spatial spatial, String indent)
     {
