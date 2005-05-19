f735a3036ed531c2d7a7fa532e583dac7e5f287a
Michael Bayne <mdb@threerings.net>
2005-05-19 00:21:50 +0000

    Reinstated the traversal of paths when pieces move.

--- src/java/com/threerings/bang/client/sprite/PieceSprite.java
+++ src/java/com/threerings/bang/client/sprite/PieceSprite.java
@@ -3,13 +3,17 @@
 
 package com.threerings.bang.client.sprite;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 
 import java.util.Iterator;
 import java.util.List;
 
 import com.jme.math.Vector3f;
+import com.threerings.jme.sprite.LinePath;
+import com.threerings.jme.sprite.LineSegmentPath;
 import com.threerings.jme.sprite.Sprite;
+import com.threerings.media.util.MathUtil;
 
 import com.threerings.bang.data.BangBoard;
 import com.threerings.bang.util.BangContext;
@@ -73,13 +77,10 @@ public class PieceSprite extends Sprite
      */
     public void setLocation (int tx, int ty, int elevation)
     {
-        float nx = TILE_SIZE * tx, ny = TILE_SIZE * ty;
-        float nz = TILE_SIZE * elevation;
-        if (localTranslation == null ||
-            nx != localTranslation.x || ny != localTranslation.y ||
-            nz != localTranslation.z) {
+        toWorldCoords(tx, ty, elevation, _temp);
+        if (!_temp.equals(localTranslation)) {
             log.info("Moving to " + tx + ", " + ty + ", " + elevation);
-            setLocalTranslation(new Vector3f(nx, ny, nz));
+            setLocalTranslation(new Vector3f(_temp));
             updateCollisionTree();
         }
     }
@@ -90,46 +91,53 @@ public class PieceSprite extends Sprite
      */
     public void updated (BangBoard board, Piece piece, short tick)
     {
+        int ox = _piece.x, oy = _piece.y;
         // note our new piece and the current tick
         Piece opiece = _piece;
         _piece = piece;
         _tick = tick;
 
         // move ourselves to our new location
-        setLocation(_piece.x, _piece.y,
-                    computeElevation(board, _piece.x, _piece.y));
+        int nx = piece.x, ny = piece.y;
+        if (nx != ox || ny != oy) {
+            int elev = computeElevation(board, _piece.x, _piece.y);
 
-//         int nx = piece.x * SQUARE, ny = piece.y * SQUARE;
-//         if (nx != _ox || ny != _oy) {
-//             if (_mgr == null || _editorMode) {
-//                 // if we're invisible just warp there
-//                 setLocation(nx, ny);
+            if (/* _mgr == null || */ _editorMode) {
+                // if we're invisible or in the editor just warp there
+                setLocation(_piece.x, _piece.y, elev);
 
-//             } else if (!isMoving()) {
-//                 List path = null;
-//                 if (board != null) {
-//                     path = board.computePath(opiece, piece.x, piece.y);
-//                 }
-//                 if (path != null) {
-//                     // convert the tile coordinates to screen coordinates
-//                     for (Iterator iter = path.iterator(); iter.hasNext(); ) {
-//                         Point p = (Point)iter.next();
-//                         p.x *= SQUARE;
-//                         p.y *= SQUARE;
-//                     }
-//                     LineSegmentPath lspath = new LineSegmentPath(path);
-//                     lspath.setVelocity(1/3f);
-//                     move(lspath);
-//                 } else {
-//                     long duration = (long)
-//                         MathUtil.distance(_ox, _oy, nx, ny) * 3;
-//                     move(new LinePath(_ox, _oy, nx, ny, duration));
-//                 }
-//             }
-
-//         } else {
-//             invalidate();
-//         }
+            // TODO: append an additional path if we're currently moving
+            } else if (!isMoving()) {
+                List path = null;
+                if (board != null) {
+                    path = board.computePath(opiece, piece.x, piece.y);
+                }
+                if (path != null) {
+                    // create a world coordinate path from the tile
+                    // coordinates
+                    Vector3f[] coords = new Vector3f[path.size()];
+                    float[] durations = new float[path.size()-1];
+                    int ii = 0, elevation = 0; // TODO: handle elevated paths
+                    for (Iterator iter = path.iterator(); iter.hasNext(); ii++) {
+                        Point p = (Point)iter.next();
+                        coords[ii] = new Vector3f();
+                        toWorldCoords(p.x, p.y, elevation, coords[ii]);
+                        if (ii > 0) {
+                            durations[ii-1] = 0.1f;
+                        }
+                    }
+                    LineSegmentPath lspath =
+                        new LineSegmentPath(this, coords, durations);
+                    move(lspath);
+                } else {
+                    Vector3f start = toWorldCoords(ox, oy, 0, new Vector3f());
+                    Vector3f end = toWorldCoords(nx, ny, 0, new Vector3f());
+                    float duration = (float)
+                        MathUtil.distance(ox, oy, nx, ny) * 3 / 1000f;
+                    move(new LinePath(this, start, end, duration));
+                }
+            }
+        }
     }
 
     /**
@@ -141,6 +149,13 @@ public class PieceSprite extends Sprite
 //         if (_mgr != null) {
 //             ((SpriteManager)_mgr).removeSprite(this);
 //         }
+    }
+
+    @Override // documentation inherited
+    public void pathCompleted ()
+    {
+        super.pathCompleted();
+        updateCollisionTree();
     }
 
 //     // documentation inherited
@@ -184,6 +199,18 @@ public class PieceSprite extends Sprite
         return 0;
     }
 
+    /**
+     * Converts tile coordinates plus elevation into (3D) world
+     * coordinates.
+     */
+    protected Vector3f toWorldCoords (int tx, int ty, int elev, Vector3f target)
+    {
+        target.x = tx * TILE_SIZE;
+        target.y = ty * TILE_SIZE;
+        target.z = elev * TILE_SIZE;
+        return target;
+    }
+
     protected Piece _piece;
     protected short _tick;
 
@@ -192,6 +219,9 @@ public class PieceSprite extends Sprite
     /** When activated, causes all pieces to warp instead of smoothly
      * follow a path. */
     protected static boolean _editorMode;
+
+    /** Used for temporary calculations. */
+    protected static Vector3f _temp = new Vector3f();
 
 //     /** Used by {@link #_computeBounds}. */
 //     protected static Rectangle _unit = new Rectangle(0, 0, SQUARE, SQUARE);
