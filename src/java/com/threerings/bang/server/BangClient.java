09a494369cbc913541c108fbff434db97b9cf46f
Michael Bayne <mdb@threerings.net>
2005-06-24 22:21:41 +0000

    Record the necessary bits when a player ends their session.

--- src/java/com/threerings/bang/server/BangClient.java
+++ src/java/com/threerings/bang/server/BangClient.java
@@ -3,6 +3,10 @@
 
 package com.threerings.bang.server;
 
+import java.util.logging.Level;
+
+import com.samskivert.util.Invoker;
+
 import com.threerings.crowd.data.TokenRing;
 import com.threerings.crowd.server.CrowdClient;
 
@@ -16,6 +20,15 @@ import static com.threerings.bang.Log.log;
  */
 public class BangClient extends CrowdClient
 {
+    @Override // documentation inherited
+    public void shutdown ()
+    {
+        super.shutdown();
+
+        // this session is over, make a note of it
+        recordEndedSession();
+    }
+
     @Override // documentation inherited
     protected void sessionWillStart ()
     {
@@ -36,5 +49,47 @@ public class BangClient extends CrowdClient
 
         // TEMP: start all players in frontier town for now
         user.townId = BangCodes.FRONTIER_TOWN;
+    }
+
+    @Override // documentation inherited
+    protected void sessionDidEnd ()
+    {
+        super.sessionDidEnd();
+
+        // this session is over, make a note of it
+        recordEndedSession();
+    }
+
+    /**
+     * Records to logs and the database anything that needs recording at
+     * the end of a session.
+     */
+    protected void recordEndedSession ()
+    {
+        final BangUserObject user = (BangUserObject)_clobj;
+        String uname = "recordEndedSession:" + user.username;
+        BangServer.invoker.postUnit(new Invoker.Unit(uname) {
+            public boolean invoke () {
+                performDatabaseSaves(user);
+                return false;
+            }
+        });
+    }
+
+    /**
+     * This method is called on the invoker thread and writes to the
+     * database any necessary information at the end of a player's
+     * session.
+     */
+    protected void performDatabaseSaves (BangUserObject user)
+    {
+        try {
+            // record our playtime to the database
+            BangServer.playrepo.noteSessionEnded(
+                user.playerId, (int)Math.round(_connectTime / 60f));
+        } catch (Exception e) {
+            log.log(Level.WARNING, "Failed to note ended session " +
+                    "[user=" + user.who() + "].", e);
+        }
     }
 }
