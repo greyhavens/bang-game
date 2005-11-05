8825cf492cb587bcca3874171b19ebf0741161ca
Michael Bayne <mdb@threerings.net>
2005-11-05 03:37:19 +0000

    Missed this little feller.

---
+++ src/java/com/threerings/bang/avatar/data/LookConfig.java
@@ -0,0 +1,27 @@
+//
+// $Id$
+
+package com.threerings.bang.avatar.data;
+
+import com.threerings.io.SimpleStreamableObject;
+
+/**
+ * Contains all the information needed to configure a new look.
+ */
+public class LookConfig extends SimpleStreamableObject
+{
+    /** The name of the new look. */
+    public String name;
+
+    /** The global hair colorization id. */
+    public int hair;
+
+    /** The global skin colorization id. */
+    public int skin;
+
+    /** The aspect selections. */
+    public String[] aspects;
+
+    /** The aspect colorizations. */
+    public int[] colors;
+}
