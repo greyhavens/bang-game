3676d0d869a9f40eb579b1074d9c62beae4072aa
Mark Johnson <mjohnson@threerings.net>
2006-07-17 23:04:16 +0000

    The trickster raven will walk distances of 3 tiles or less instead of turning into the raven.

--- src/java/com/threerings/bang/game/client/sprite/TricksterRavenSprite.java
+++ src/java/com/threerings/bang/game/client/sprite/TricksterRavenSprite.java
@@ -3,7 +3,12 @@
 
 package com.threerings.bang.game.client.sprite;
 
+import java.util.Iterator;
 import java.util.List;
+
+import java.awt.Point;
+
+import com.jme.math.Vector3f;
 
 import com.threerings.bang.game.data.BangBoard;
 
@@ -22,7 +27,27 @@ public class TricksterRavenSprite extends UnitSprite
     @Override // documentation inherited
     protected Path createPath (BangBoard board, List path, float speed)
     {
-        setMoveType((path.size() > 2) ? MOVE_FLYING : MOVE_WALKING);
+        boolean walking = path.size() <= 4;
+        if (walking) {
+            for (Iterator iter = path.iterator(); iter.hasNext(); ) {
+                Point p = (Point)iter.next();
+                if (!board.isTraversable(p.x, p.y)) {
+                    walking = false;
+                    break;
+                }
+            }
+        }
+        setMoveType(walking ? MOVE_WALKING : MOVE_FLYING);
         return super.createPath(board, path, speed);
+    }
+
+    @Override // documentation inherited
+    protected void setCoord (BangBoard board, Vector3f[] coords, int idx,
+                             int nx, int ny, boolean moving)
+    {
+        if (MOVE_WALKING.equals(getMoveType())) {
+            moving = false;
+        }
+        super.setCoord(board, coords, idx, nx, ny, moving);
     }
 }
