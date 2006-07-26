01f649dbf6b9d69f22dee6c8093f7c0f6c0252ee
Andrzej Kapolka <andrzej@threerings.net>
2006-07-26 22:59:43 +0000

    Logging robots hurt adjacent units with their saw blades.

--- src/java/com/threerings/bang/game/data/piece/LoggingRobot.java
+++ src/java/com/threerings/bang/game/data/piece/LoggingRobot.java
@@ -3,8 +3,13 @@
 
 package com.threerings.bang.game.data.piece;
 
+import java.util.ArrayList;
+
 import com.threerings.bang.game.client.sprite.MobileSprite;
 import com.threerings.bang.game.client.sprite.PieceSprite;
+import com.threerings.bang.game.data.BangBoard;
+import com.threerings.bang.game.data.effect.Effect;
+import com.threerings.bang.game.data.effect.ShotEffect;
 
 /**
  * The logging robot for the forest guardians scenario.
@@ -17,7 +22,25 @@ public class LoggingRobot extends BallisticUnit
         return BASE_TREE_PROXIMITY_DAMAGE;
     }
     
+    @Override // documentation inherited
+    public ArrayList<Effect> tick (short tick, BangBoard board, Piece[] pieces)
+    {
+        ArrayList<Effect> effects = new ArrayList<Effect>();
+        for (Piece piece : pieces) {
+            if (piece instanceof Unit && getDistance(piece) == 1 &&
+                validTarget(piece, false)) {
+                effects.add(new ShotEffect(this, piece, UNIT_PROXIMITY_DAMAGE,
+                    null, null));
+            }
+        }
+        return effects.isEmpty() ? null : effects;
+    }
+    
     /** The base amount by which logging robots next to trees increase their
      * damage and encourage them to shrink. */
     public static final int BASE_TREE_PROXIMITY_DAMAGE = 20;
+    
+    /** The base amount by which logging robots next to units damage them with
+     * their rotating saw blades. */
+    public static final int UNIT_PROXIMITY_DAMAGE = 5;
 }
