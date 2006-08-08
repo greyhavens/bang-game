37b36329557db43aa8c1960d9939ab5d3df6739b
Michael Bayne <mdb@threerings.net>
2006-08-08 19:06:10 +0000

    Drive by whitespace patrol (I'm going to modify this but I figured I'd do the whitespace removal first).

--- src/java/com/threerings/bang/data/PardnerEntry.java
+++ src/java/com/threerings/bang/data/PardnerEntry.java
@@ -19,32 +19,32 @@ public class PardnerEntry
 {
     /** The pardner is not logged in. */
     public static final byte OFFLINE = 0;
-    
+
     /** The pardner is somewhere other than the saloon or a game. */
     public static final byte ONLINE = 1;
-    
+
     /** The pardner is in the saloon interface. */
     public static final byte IN_SALOON = 2;
-    
+
     /** The pardner is in a game. */
     public static final byte IN_GAME = 3;
-    
+
     /** The pardner's handle. */
     public Handle handle;
-    
+
     /** The pardner's avatar. */
     public int[] avatar;
-    
+
     /** The pardner's status ({@link #OFFLINE}, {@link #IN_TOWN}, etc). */
     public byte status;
-    
+
     /**
      * No-arg constructor for deserialization.
      */
     public PardnerEntry ()
     {
     }
-    
+
     /**
      * Constructor for online pardners.
      */
@@ -52,7 +52,7 @@ public class PardnerEntry
     {
         this.handle = (handle == null) ? new Handle("") : handle;
     }
-    
+
     /**
      * Constructor for offline pardners.
      */
@@ -61,7 +61,7 @@ public class PardnerEntry
         this(handle);
         setLastSession(lastSession);
     }
-    
+
     /**
      * Determines whether this pardner is online.
      */
@@ -69,7 +69,7 @@ public class PardnerEntry
     {
         return status != OFFLINE;
     }
-    
+
     /**
      * Determines whether this pardner is available for chat (i.e., online and
      * not in a game).
@@ -78,7 +78,7 @@ public class PardnerEntry
     {
         return status != OFFLINE && status != IN_GAME;
     }
-    
+
     /**
      * Retrieves the date at which the pardner was last online.
      */
@@ -87,7 +87,7 @@ public class PardnerEntry
         return new Date(_lastSessionEpoch +
             _lastSession * MILLISECONDS_PER_DAY);
     }
-    
+
     /**
      * Sets the date at which the pardner was last online.
      */
@@ -96,13 +96,13 @@ public class PardnerEntry
         _lastSession = (short)((lastSession.getTime() - _lastSessionEpoch) /
             MILLISECONDS_PER_DAY);
     }
-    
+
     // documentation inherited from interface DSet.Entry
     public Comparable getKey ()
     {
         return handle;
     }
-    
+
     // documentation inherited from interface Comparable
     public int compareTo (Object other)
     {
@@ -111,19 +111,19 @@ public class PardnerEntry
         PardnerEntry oentry = (PardnerEntry)other;
         if (isOnline() != oentry.isOnline()) {
             return isOnline() ? -1 : +1;
-        
+
         } else if (isAvailable() != oentry.isAvailable()) {
             return isAvailable() ? -1 : +1;
-            
+
         } else {
             return handle.compareTo(oentry.handle);
         }
     }
-    
+
     /** For offline pardners, the date when the pardner last logged on, stored
      * as the number of days since midnight, 1/1/05 GMT. */
     protected short _lastSession;
-    
+
     /** The time from which last session times are measured. */
     protected static long _lastSessionEpoch;
     static {
@@ -131,7 +131,7 @@ public class PardnerEntry
         cal.set(2005, 0, 1, 0, 0, 0);
         _lastSessionEpoch = cal.getTimeInMillis();
     }
-    
+
     /** The number of milliseconds in a day. */
     protected static final long MILLISECONDS_PER_DAY = 24L * 60 * 60 * 1000;
 }
