97f7dd9f7181d987745d6edea50a729c1af5a4ae
Andrzej Kapolka <andrzej@threerings.net>
2006-08-25 19:13:50 +0000

    Start the editor's "game" when the controller reports its readiness.

--- src/java/com/threerings/bang/editor/EditorManager.java
+++ src/java/com/threerings/bang/editor/EditorManager.java
@@ -3,7 +3,9 @@
 
 package com.threerings.bang.editor;
 
+import com.threerings.crowd.data.BodyObject;
 import com.threerings.crowd.data.PlaceObject;
+import com.threerings.parlor.game.data.GameObject;
 import com.threerings.parlor.game.server.GameManager;
 
 import com.threerings.bang.game.data.BangObject;
@@ -13,6 +15,15 @@ import com.threerings.bang.game.data.BangObject;
  */
 public class EditorManager extends GameManager
 {
+    @Override // documentation inherited
+    public void playerReady (BodyObject caller)
+    {
+        // caller is null because the message originated on the server;
+        // start things up directly
+        gameWillStart();
+        _gameobj.setState(GameObject.IN_PLAY);
+    }
+    
     // documentation inherited
     protected PlaceObject createPlaceObject ()
     {
