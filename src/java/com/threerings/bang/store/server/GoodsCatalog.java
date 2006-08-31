b4ce3393f5fd84b430b95c242340231293144dbe
Andrzej Kapolka <andrzej@threerings.net>
2006-08-31 18:29:56 +0000

    Lowered the card pack prices.  The ones that require coins are still ridiculous at the current exchange rate, but the five card pack is cheap.

--- src/java/com/threerings/bang/store/server/GoodsCatalog.java
+++ src/java/com/threerings/bang/store/server/GoodsCatalog.java
@@ -239,8 +239,8 @@ public class GoodsCatalog
 
     /** Quantity, scrip cost and coin cost for our packs of cards. */
     protected static final int[] PACK_PRICES = {
-        5, 300, 1,
-        13, 750, 2,
-        52, 2500, 6,
+        5, 125, 0,
+        13, 200, 1,
+        52, 700, 3,
     };
 }
