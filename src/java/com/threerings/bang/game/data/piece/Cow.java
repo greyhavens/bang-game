4420c18a31d80ae2f8cba8fbe06d7fc85a8bc945
Mark Johnson <mjohnson@threerings.net>
2006-09-20 01:25:30 +0000

    Yay for namespace collisions causing confusion!!

--- src/java/com/threerings/bang/game/data/piece/Cow.java
+++ src/java/com/threerings/bang/game/data/piece/Cow.java
@@ -48,7 +48,7 @@ public class Cow extends Piece
                     bangobj.grantPoints(
                         this.owner, -CattleRustlingInfo.POINTS_PER_COW);
                 }
-                setOwner(bangobj, spooker.owner);
+                owner = spooker.owner;
                 bangobj.grantPoints(owner, CattleRustlingInfo.POINTS_PER_COW);
             }
         }
