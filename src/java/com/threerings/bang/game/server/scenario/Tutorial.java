50064675de3c9bd933ebe3ee93449768b1b260c5
Michael Bayne <mdb@threerings.net>
2006-10-31 19:19:53 +0000

    Don't freak out if we don't specify a target with move_unit.

--- src/java/com/threerings/bang/game/server/scenario/Tutorial.java
+++ src/java/com/threerings/bang/game/server/scenario/Tutorial.java
@@ -270,7 +270,7 @@ public class Tutorial extends Scenario
 
             // if the target is not shootable, we want just to move next to it
             Piece target = _bangobj.pieces.get(targetId);
-            if (!unit.validTarget(_bangobj, target, false)) {
+            if (target != null && !unit.validTarget(_bangobj, target, false)) {
                 targetId = -1;
                 tx = target.x;
                 ty = target.y;
