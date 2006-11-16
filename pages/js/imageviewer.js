5e5a95db4d7b0e7b8cbfcda7e22b77c8f5ca66e5
Michelle <michelle@threerings.net>
2006-11-16 22:48:15 +0000

    Blocking off a more space-efficient layout for town pages: - Town navbar is narrower, enhanced the colors for selected town. - About panel is also shorter, placeholder screenshots put in.. still needs bit more bling + thinking of taking off even more height in the next rev.

--- pages/js/imageviewer.js
+++ pages/js/imageviewer.js
@@ -1,3 +1,8 @@
+function show(image) {
+  window.open("/screenshots/2006/frontier/shot.html?" + image, "image",
+    "resizable=yes,status=no,scrollbars=yes");
+  return false;
+}
 
 function showImage(imagename) {
     document.thisimage.src = imagename;
