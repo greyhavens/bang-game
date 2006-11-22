ad2b001a5b1d479a2ab299198a0c13f46cc67e12
Michelle <michelle@threerings.net>
2006-11-22 01:00:20 +0000

    Gallery tweaks for Frontier Town: Renamed the test screenshots, updated galleries to include previous and next functionality (text links for now, working on graphics), removed new window when clicking on thumbnail.

--- pages/js/imageviewer.js
+++ pages/js/imageviewer.js
@@ -1,9 +1,96 @@
-function show(image) {
-  window.open("/screenshots/2006/frontier/shot.html?" + image, "image",
-    "resizable=yes,status=no,scrollbars=yes");
-  return false;
+// ------------------------------------------------------------------
+// Figure out which subpage we are on
+var query = window.location.search.substring(1);
+var vars = query.split("&");
+for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split(",");
+}
+var queryCurrentPage = pair[0];
+var queryTotalPages = pair[1];
+	
+if (!queryCurrentPage) {
+    queryCurrentPage = 1;
+    queryTotalPages = 1;
 }
 
-function showImage(imagename) {
-    document.thisimage.src = imagename;
+// ------------------------------------------------------------------
+// Layout the Nav	
+function printThumbNav() {	
+    var sPath = window.location.pathname;
+    var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+        
+    if (queryCurrentPage == 1) {
+        var prevPage = 1;
+    } else {
+        var prevPage = parseInt(queryCurrentPage) - 1;
+    }
+    
+    if (queryCurrentPage == pages) {
+        var nextPage = pages;
+    } else {
+        var nextPage = parseInt(queryCurrentPage) + 1;
+    }
+    document.write ('<table width="700px" border="0"><tr><td>');
+    document.write('<a href="?' + prevPage + ',' + pages + '">Prev</a>');
+    document.write ('</td><td align="center">');
+    if (pages != 1) {
+        for (x = 1; x<=pages; x++) {
+            document.write("<a href =" + sPage +"?"+ x +","+ pages +">");
+    
+            if (x == currentPage) {
+                document.write("<font color = #330000><b>");
+            }
+            document.write(x);
+            if (x == currentPage) {
+                document.write("</b></font>");
+            }
+            document.write("</a>");
+            if (x != pages) {
+                document.write(" | ");
+            }
+        }
+    }
+    document.write ('</td><td align="right">');
+    document.write('<a href="?' + nextPage + '@' + pages + '">Next</a>');
+    document.write ('</td></tr></table>');
 }
+
+// ------------------------------------------------------------------
+// Layout the Gallery
+function buildGallery() {
+//Figure out the first img for the page
+    var theStart = screenshotsTotal - ((currentPage - 1) * screenshotsPerPage);	
+	
+	//Check if you have enough pics to fill the full grid
+	var theEnd = 0;
+	if (theStart - screenshotsPerPage > 0) {
+	    theEnd = theStart - screenshotsPerPage;
+	}
+	
+	//Build the table of thumbs
+	var whichColumn = 1;
+	while (theStart > theEnd) {
+		if (whichColumn == 1) {
+			document.write("<tr>");
+		}
+		document.write("<td>");
+		screenshot(theStart);
+		document.write("</td>");
+		if (whichColumn == columns) {
+			document.write("</tr>");
+			whichColumn = 1;
+		} else 
+			whichColumn++;
+		theStart--;
+	}
+}
+
+// ------------------------------------------------------------------
+// Layout the specific screenshot
+
+function screenshot(number) {
+	document.write('<a href="index2.html?'+ImgLibrary+','+number+','+screenshotsTotal+'"><img src="' + ImgLibrary + 'thumbs/' + number +'.png" border="0"></a>');
+}
+
+
+
---
+++ pages/js/imageviewer2.js
@@ -0,0 +1,168 @@
+// ------------------------------------------------------------------
+// Grab all the variables from the URL and figure out what they are
+
+var sPath = window.location.pathname;
+
+var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split(",");
+}
+	var imgDir = pair[0];
+	var screenshotCurrent = pair[1];
+	var screenshotsTotal = pair[2];
+
+var screenshotNext = eval(screenshotCurrent) + 1;
+var screenshotPrevious = screenshotCurrent - 1;
+
+if (screenshotPrevious == 0)
+	screenshotPrevious = screenshotsTotal;
+if (screenshotNext > screenshotsTotal)
+	screenshotNext = 1;	
+
+// ------------------------------------------------------------------
+
+function previousPicture(){
+
+   if (document.all){
+      document.images.SlideShow.style.filter="blendTrans(duration=2)"
+      document.images.SlideShow.style.filter="blendTrans(duration=crossFadeDuration)"
+      document.images.SlideShow.filters.blendTrans.Apply()      
+   }
+
+	if (screenshotsTotal > 2)
+		document.images.SlideShow.src = preLoad[1].src
+	else if (screenshotsTotal == 1)
+		document.images.SlideShow.src = preLoad[0].src
+	else 
+		document.images.SlideShow.src = preLoad[1].src   
+
+
+   if (document.all){
+      document.images.SlideShow.filters.blendTrans.Play()
+   }
+
+	var blah = 0;
+	var temp;
+	Pic[blah] = imgDir + screenshotNext +'.png';
+	blah ++;
+	
+	temp = screenshotCurrent;
+	screenshotCurrent = screenshotNext;
+	screenshotPrevious = temp;
+	screenshotNext++;
+	if (screenshotNext > screenshotsTotal)
+		screenshotNext = 1;	
+	Pic[blah] = imgDir + screenshotNext +'.png';
+	blah++;
+	Pic[blah] = imgDir + screenshotPrevious +'.png';   
+
+	if (checkTotal != screenshotsTotal || check[screenshotNext-1] == 1) {
+		preLoad[1] = new Image();
+		check[screenshotNext-1] = 0;
+		checkTotal++;
+	}
+
+	if (screenshotsTotal > 2) {
+		preLoad[0].src = Pic[0];
+		preLoad[1].src = Pic[1];
+		preLoad[2].src = Pic[2];		
+	}
+	else if (screenshotsTotal == 1)
+		preLoad[0].src = Pic[0];
+	else 
+		preLoad[1].src = Pic[1];		
+	   
+	displayScreenshotCurrent = screenshotsTotal - screenshotCurrent + 1;
+	document.getElementById('displayCurrent').innerHTML = displayScreenshotCurrent;
+	document.getElementById('displayCurrent').style.fontWeight = "bold";	
+	document.getElementById('displayCurrent').style.color = "#FFFFFF";	
+	
+	return false;
+}
+
+// ------------------------------------------------------------------
+function nextPicture(){
+
+   if (document.all){
+      document.images.SlideShow.style.filter="blendTrans(duration=2)"
+      document.images.SlideShow.style.filter="blendTrans(duration=crossFadeDuration)"
+      document.images.SlideShow.filters.blendTrans.Apply()      
+   }
+
+	if (screenshotsTotal > 2)
+	   document.images.SlideShow.src = preLoad[2].src
+	else if (screenshotsTotal == 1)
+		document.images.SlideShow.src = preLoad[0].src
+	else 
+		document.images.SlideShow.src = preLoad[1].src
+
+
+   if (document.all){
+      document.images.SlideShow.filters.blendTrans.Play()
+   }
+
+	var temp;
+
+	temp = screenshotCurrent;
+	screenshotCurrent = screenshotPrevious;
+	screenshotNext = temp;
+	screenshotPrevious--;
+	if (screenshotPrevious == 0)
+		screenshotPrevious = screenshotsTotal;	
+		
+	var blah = 0;
+	Pic[blah] = imgDir + directory + screenshotCurrent +'.png';
+	blah ++;
+	Pic[blah] = imgDir + directory + screenshotNext +'.png';
+	blah++;
+	Pic[blah] = imgDir + directory + screenshotPrevious +'.png';   
+
+
+	if (checkTotal != screenshotsTotal || check[screenshotPrevious-1] == 1) {
+		preLoad[2] = new Image();
+		check[screenshotPrevious-1] = 0;
+		checkTotal++;
+	}
+
+	if (screenshotsTotal > 2){
+		preLoad[0].src = Pic[0];
+		preLoad[1].src = Pic[1];
+		preLoad[2].src = Pic[2];
+	}
+	else if (screenshotsTotal == 1)
+		preLoad[0].src = Pic[0];
+	else 
+		preLoad[1].src = Pic[1];		
+
+
+	displayScreenshotCurrent = screenshotsTotal - screenshotCurrent + 1;		
+	document.getElementById('displayCurrent').innerHTML = displayScreenshotCurrent;
+	document.getElementById('displayCurrent').style.fontWeight = "bold";	
+	document.getElementById('displayCurrent').style.color = "#ffffff";	
+		   
+		   
+	return false;
+}
+// ------------------------------------------------------------------
+
+//Layout the Nav
+function printGalleryNav() {	
+    document.write ('<table width="700px" border="0"><tr><td width="150px">');
+    // Back button to go to the previous graphic
+    //document.write ('<a href="'+sPath+'?'+imgDir+','+screenshotNext+','+screenshotsTotal+'"><img src = "'+ imgDir +'back.png" border="0" name="SlideShow" alt="Click on image to view next entry"></a>');
+    document.write ('<a href="'+sPath+'?'+imgDir+','+screenshotNext+','+screenshotsTotal+'">Previous</a>');
+    document.write ('</td><td align="center" >');
+    // Tell the user which image they are on, and how many photos in the full collection
+    var displayScreenshotCurrent = screenshotsTotal - screenshotCurrent + 1;
+    document.write ('<b><span id = "displayCurrent" style = "font-weight: bold; color: #330000;">'+ displayScreenshotCurrent +'</span> of '+ screenshotsTotal + '</b><br/>');
+    // Back to thumbnail gallery 
+    document.write ('<a href = "'+ upURL +'">Back to Thumbnail Gallery</a></span>');
+    document.write ('</td><td align="right" width="150px">');
+    // Next button to go to the next graphic
+    //document.write ('<a href="'+sPath+'?'+imgDir+','+screenshotPrevious+','+screenshotsTotal+'"><img src = "'+ imgDir +'next.png" border="0" name="SlideShow" alt="Click on image to view next entry"></a>');
+    document.write ('<a href="'+sPath+'?'+imgDir+','+screenshotPrevious+','+screenshotsTotal+'">Next</a>');
+    document.write ('</td></tr></table>');
+}
+
+
