0003cd1536ec962623403461225d086702aa831a
Andrzej Kapolka <andrzej@threerings.net>
2006-11-27 22:48:45 +0000

    Added new wendigo effects (snow storm, ice shards).

--- src/java/com/threerings/bang/game/client/WendigoHandler.java
+++ src/java/com/threerings/bang/game/client/WendigoHandler.java
@@ -3,10 +3,21 @@
 
 package com.threerings.bang.game.client;
 
+import com.jme.math.FastMath;
+import com.jme.math.Vector3f;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Controller;
+import com.jme.scene.state.MaterialState;
+
 import com.samskivert.util.Interval;
 
+import com.threerings.jme.model.Model;
+import com.threerings.jme.sprite.Sprite;
 import com.threerings.openal.Sound;
 
+import com.threerings.bang.client.util.ResultAttacher;
+
+import com.threerings.bang.game.client.sprite.PieceSprite;
 import com.threerings.bang.game.client.sprite.WendigoSprite;
 
 import com.threerings.bang.game.data.BangObject;
@@ -17,6 +28,7 @@ import com.threerings.bang.game.data.effect.WendigoEffect.Collision;
 import com.threerings.bang.game.data.piece.Piece;
 
 import static com.threerings.bang.Log.log;
+import static com.threerings.bang.client.BangMetrics.*;
 
 /**
  * Displays a wendigo.
@@ -30,6 +42,10 @@ public class WendigoHandler extends EffectHandler
 
         // play the flight sound
         _sounds.getSound(SOUND_PATH + "flight.ogg").play(true);
+        
+        // fire off the snow storm effect
+        _view.displayCameraParticles("indian_post/wendigo/snow_storm",
+            CAMERA_EFFECT_DURATION);
         
         long delay = 0;
         for (WendigoEffect.Movement m : effect.moves) {
@@ -116,8 +132,7 @@ public class WendigoHandler extends EffectHandler
                     apply(_collision.deathEffect);
                 }        
                 if (target != null) {
-                    Effect.damage(_bangobj, WendigoHandler.this, -1, null,
-                        target, 100, ShotEffect.DAMAGED);
+                    new ShardSprite(target).activate();
                 }
             } else {
                 _sounds.getSound(SOUND_PATH + "defense.ogg").play(true);
@@ -133,7 +148,70 @@ public class WendigoHandler extends EffectHandler
         protected Collision _collision;
     }
 
+    /** Handles the exploding ice shard effect. */
+    protected class ShardSprite extends Sprite
+    {
+        public ShardSprite (Piece target)
+        {
+            _target = target;
+            _ctx.loadModel("extras", "indian_post/ice_shard",
+                new ResultAttacher<Model>(this));
+        }
+        
+        /**
+         * Activates the shard effect.
+         */
+        public void activate ()
+        {
+            final PieceSprite sprite = _view.getPieceSprite(_target);
+            if (sprite == null) {
+                effectDamage();
+                return;
+            }
+            sprite.getModelNode().pauseAnimation(true);
+            sprite.attachChild(this);
+            final int penderId = notePender();
+            final MaterialState mstate = _ctx.getRenderer().createMaterialState();
+            mstate.getAmbient().set(ColorRGBA.white);
+            mstate.getDiffuse().set(1f, 1f, 1f, 0f);
+            setRenderState(mstate);
+            updateRenderState();
+            addController(new Controller() {
+                public void update (float time) {
+                    if ((_elapsed += time) >=
+                        SHARD_RISE_DURATION + SHARD_LINGER_DURATION) {
+                        sprite.detachChild(ShardSprite.this);
+                        sprite.displayParticles(
+                            "indian_post/wendigo/ice_shard_explosion", true);
+                        sprite.getModelNode().pauseAnimation(false);
+                        effectDamage();
+                        maybeComplete(penderId);
+                    }
+                    float alpha = Math.min(1f, _elapsed / SHARD_RISE_DURATION);
+                    mstate.getDiffuse().a = alpha;
+                    getLocalTranslation().set(0f, 0f, TILE_SIZE * (alpha - 1));
+                    getLocalRotation().fromAngleNormalAxis(FastMath.PI * alpha,
+                        Vector3f.UNIT_Z);
+                }
+                protected float _elapsed;
+            });
+        }
+        
+        protected void effectDamage ()
+        {
+            Effect.damage(_bangobj, WendigoHandler.this, -1, null,
+                _target, 100, ShotEffect.DAMAGED);
+        }
+        
+        protected Piece _target;
+        protected PieceSprite _tsprite;
+    }
+    
     protected static final long WENDIGO_SPACING = 150;
+    
+    protected static final float SHARD_RISE_DURATION = 1.5f;
+    
+    protected static final float SHARD_LINGER_DURATION = 0.5f;
     
     protected static final String SOUND_PATH =
         "rsrc/extras/indian_post/wendigo/";
