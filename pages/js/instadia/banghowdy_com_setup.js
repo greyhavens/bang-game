5c69a7b9de40a6ec8c48d867dc4ffc96b81447fb
Elizabeth Fong <elizabeth@threerings.net>
2006-12-06 11:29:34 +0000

    add instadia tracking

---
+++ pages/js/instadia/banghowdy_com_setup.js
@@ -0,0 +1,13 @@
+/*
+* Instadia default tracking variables
+*/
+
+var CSid = '012BR';		// Customer ID
+
+/*
+* Instadia specials setup variables...
+*/
+
+var CSdl = true;		// Boolean switch to turn off/on (false/true) tracking of File Downloads
+var CSlt = false;		// Boolean switch to turn off/on (false/true) using the link text instead of the filename as tracking title
+var CSel = true;		// Boolean switch to turn off/on (false/true) tracking of Exit Links
