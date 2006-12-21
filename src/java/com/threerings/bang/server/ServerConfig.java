6f4c8a304e98cb0fdf2f2b179a3a4518bdea98c1
Michael Bayne <mdb@threerings.net>
2006-12-21 03:42:17 +0000

    Some config file location jockeying. Things are now simpler.

--- src/java/com/threerings/bang/server/ServerConfig.java
+++ src/java/com/threerings/bang/server/ServerConfig.java
@@ -29,12 +29,10 @@ public class ServerConfig
     /** The name assigned to this server node. */
     public static String nodename;
 
-    /** The back-channel DNS name of the host on which this server is
-     * running. */
+    /** The back-channel DNS name of the host on which this server is running. */
     public static String hostname;
 
-    /** The publicly accessible DNS name of the host on which this server is
-     * running. */
+    /** The publicly accessible DNS name of the host on which this server is running. */
     public static String publicHostname;
 
     /** The id of the town this server is handling. */
@@ -43,9 +41,8 @@ public class ServerConfig
     /** The index of the town this server is handling. */
     public static int townIndex;
 
-    /** Eventually we'll have servers for the "town" and servers for games
-     * started from that town. For now all servers are town servers and games
-     * are played on the town server. */
+    /** Eventually we'll have servers for the "town" and servers for games started from that
+     * town. For now all servers are town servers and games are played on the town server. */
     public static boolean isTownServer = true;
 
     /** The secret used to authenticate other servers in our cluster. */
@@ -54,9 +51,8 @@ public class ServerConfig
     /** The root directory of the server installation. */
     public static File serverRoot;
 
-    /** Provides access to our config properties. <em>Do not</em> modify
-     * these properties! */
-    public static Config config;
+    /** Provides access to our config properties. <em>Do not</em> modify these properties! */
+    public static Config config = new Config("server");
 
     /**
      * Returns the JDBC configuration.
@@ -67,8 +63,8 @@ public class ServerConfig
     }
 
     /**
-     * Instantiates and returns the authenticator that the server will use
-     * to authenticate client connections.
+     * Instantiates and returns the authenticator that the server will use to authenticate client
+     * connections.
      */
     public static BangAuthenticator getAuthenticator ()
     {
@@ -85,57 +81,33 @@ public class ServerConfig
     }
 
     /**
-     * Configures the install config with the path to our installation
-     * properties file. This method is called automatically.
+     * Configures server bits when this class is resolved.
      */
-    protected static void init (String propPath)
-    {
-        Properties props = new Properties();
-        try {
-            if (propPath != null) {
-                propPath = propPath + File.separator + "server.properties";
-                props.load(new FileInputStream(propPath));
-            } else {
-                propPath = "server.properties";
-                props.load(ServerConfig.class.getClassLoader().
-                           getResourceAsStream(propPath));
-            }
-        } catch (Exception e) {
-            log.log(Level.WARNING, "Failed to load install properties " +
-                    "[path=" + propPath + "].", e);
-        }
-        config = new Config("server", props);
-
+    static {
         // fill in our standard properties
         serverRoot = new File(config.getValue("server_root", "/tmp"));
         sharedSecret = config.getValue("server_secret", (String)null);
 
-        // if we're not a server node (we're the webapp or a tool) stop here
-        if (!Boolean.getBoolean("is_node")) {
-            return;
-        }
+        // if we're a server node (not the webapp or a tool) do some extra stuff
+        if (Boolean.getBoolean("is_node")) {
+            // our server name and hostname is our node identifier and comes from a system property
+            // passed by our startup scripts
+            nodename = System.getProperty("node");
+            if (StringUtil.isBlank(nodename)) {
+                log.warning("Missing 'node' system property. Cannot start.");
+            }
+            hostname = System.getProperty("hostname");
+            if (StringUtil.isBlank(hostname)) {
+                log.warning("Missing 'hostname' system property. Cannot start.");
+            }
+            if (StringUtil.isBlank(nodename) || StringUtil.isBlank(hostname)) {
+                System.exit(-1);
+            }
 
-        // our server name and hostname is our node identifier and comes from a
-        // system property passed by our startup scripts
-        nodename = System.getProperty("node");
-        if (StringUtil.isBlank(nodename)) {
-            log.warning("Missing 'node' system property. Cannot start.");
+            // fill in our node-specific properties
+            publicHostname = config.getValue(nodename + ".server_host", hostname);
+            townId = config.getValue(nodename + ".town_id", BangCodes.FRONTIER_TOWN);
+            townIndex = BangUtil.getTownIndex(townId);
         }
-        hostname = System.getProperty("hostname");
-        if (StringUtil.isBlank(hostname)) {
-            log.warning("Missing 'hostname' system property. Cannot start.");
-        }
-        if (StringUtil.isBlank(nodename) || StringUtil.isBlank(hostname)) {
-            System.exit(-1);
-        }
-
-        // fill in our node-specific properties
-        publicHostname = config.getValue(nodename + ".server_host", hostname);
-        townId = config.getValue(nodename + ".town_id", BangCodes.FRONTIER_TOWN);
-        townIndex = BangUtil.getTownIndex(townId);
-    }
-
-    static {
-        init(System.getProperty("bang.home"));
     }
 }
