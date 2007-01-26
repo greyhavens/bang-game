3344ecb14970e5193ba80d3d994827d7529a58b2
Michael Bayne <mdb@threerings.net>
2007-01-26 01:36:46 +0000

    Don't steal nuggets from team members' claims. Widened.

--- src/java/com/threerings/bang/game/server/ai/GoldLogic.java
+++ src/java/com/threerings/bang/game/server/ai/GoldLogic.java
@@ -28,38 +28,34 @@ public class GoldLogic extends AILogic
     /**
      * Creates the AI logic for a gold-based scenario.
      *
-     * @param stealing whether or not units can steal nuggets from claims
-     * (i.e., whether this is for the claim jumping scenario)
+     * @param stealing whether or not units can steal nuggets from claims (i.e., whether this is
+     * for the claim jumping scenario)
      */
     public GoldLogic (boolean stealing)
     {
         _stealing = stealing;
     }
-    
+
     // documentation inherited
     public String getBigShotType ()
     {
         // prefer a big shot with greater move distance
-        UnitConfig[] configs = UnitConfig.getTownUnits(_bangobj.townId,
-            UnitConfig.Rank.BIGSHOT);
+        UnitConfig[] configs = UnitConfig.getTownUnits(_bangobj.townId, UnitConfig.Rank.BIGSHOT);
         return getWeightedUnitTypes(configs, OFFENSE_EVALUATOR, 1)[0];
     }
 
     // documentation inherited
     public String[] getUnitTypes (int count)
     {
-        UnitConfig[] configs = UnitConfig.getTownUnits(_bangobj.townId,
-            UnitConfig.Rank.NORMAL);
+        UnitConfig[] configs = UnitConfig.getTownUnits(_bangobj.townId, UnitConfig.Rank.NORMAL);
         return getWeightedUnitTypes(configs, OFFENSE_EVALUATOR, count);
     }
-    
+
     // documentation inherited
-    protected void moveUnit (
-        Piece[] pieces, Unit unit, PointSet moves, PointSet attacks)
+    protected void moveUnit (Piece[] pieces, Unit unit, PointSet moves, PointSet attacks)
     {
-        // search for own claim, closest enemy claim with nuggets,
-        // closest enemy with nugget, closest free nugget, and enemies
-        // near our claim
+        // search for own claim, closest enemy claim with nuggets, closest enemy with nugget,
+        // closest free nugget, and enemies near our claim
         Counter oclaim = null, cclaim = null;
         Unit ctarget = null;
         Piece cnugget = null, tporter = null;
@@ -69,110 +65,103 @@ public class GoldLogic extends AILogic
                 Counter claim = (Counter)pieces[ii];
                 if (claim.owner == _pidx) {
                     oclaim = claim;
-                } else if (_stealing && claim.count > 0 && (cclaim == null ||
-                    unit.getDistance(claim) < unit.getDistance(cclaim))) {
+                } else if (_stealing && claim.count > 0 &&
+                           claim.getTeam(_bangobj) != _bangobj.getTeam(_pidx) &&
+                           (cclaim == null || unit.getDistance(claim) < unit.getDistance(cclaim))) {
                     cclaim = claim;
                 }
+
             } else if (Bonus.isBonus(pieces[ii], NuggetEffect.NUGGET_BONUS)) {
-                if (cnugget == null || unit.getDistance(pieces[ii]) <
-                        unit.getDistance(cnugget)) {
+                if (cnugget == null || unit.getDistance(pieces[ii]) < unit.getDistance(cnugget)) {
                     cnugget = pieces[ii];
                 }
-            } else if (pieces[ii] instanceof Unit &&
-                pieces[ii].owner != _pidx) {
+
+            } else if (pieces[ii] instanceof Unit && pieces[ii].owner != _pidx) {
                 Unit target = (Unit)pieces[ii];
-                if (NuggetEffect.NUGGET_BONUS.equals(target.holding) && 
+                if (NuggetEffect.NUGGET_BONUS.equals(target.holding) &&
                     (ctarget == null ||
                      unit.getDistance(target) < unit.getDistance(ctarget)) &&
-                     unit.validTarget(_bangobj, target, false)) {
+                    unit.validTarget(_bangobj, target, false)) {
                     ctarget = target;
                 }
-                if (_stealing && _claimloc != null && target.getDistance(
-                    _claimloc.x, _claimloc.y) <= DEFENSIVE_PERIMETER) {
-                    breached = true;                
+                if (_stealing && _claimloc != null &&
+                    target.getDistance(_claimloc.x, _claimloc.y) <= DEFENSIVE_PERIMETER) {
+                    breached = true;
                 }
+
             } else if (pieces[ii] instanceof Teleporter && (tporter == null ||
-                unit.getDistance(pieces[ii]) < unit.getDistance(tporter))) {
+                       unit.getDistance(pieces[ii]) < unit.getDistance(tporter))) {
                 tporter = pieces[ii];
             }
         }
+
         if (oclaim == null) {
-            log.warning("Missing own counter for AI unit [where=" +
-                _bangmgr.where() + ", pidx=" + _pidx + "].");
+            log.warning("Missing own counter for AI unit [where=" + _bangmgr.where() +
+                        ", pidx=" + _pidx + "].");
             return;
-            
-        } else if (_claimloc == null) {
+        }
+        if (_claimloc == null) {
             _claimloc = new Point(oclaim.x, oclaim.y);
         }
-        
+
         // if we have a nugget or our claim is in danger, haul ass back home
-        if ((NuggetEffect.NUGGET_BONUS.equals(unit.holding) || 
-             (breached && oclaim.count > 0 &&
-              unit.getDistance(oclaim) > DEFENSIVE_PERIMETER)) &&
+        if ((NuggetEffect.NUGGET_BONUS.equals(unit.holding) ||
+             (breached && oclaim.count > 0 && unit.getDistance(oclaim) > DEFENSIVE_PERIMETER)) &&
             moveUnit(pieces, unit, moves, oclaim, 1)) {
-            return;
-        
+
         // if there's a nugget within reach, grab it
         } else if (cnugget != null && moves.contains(cnugget.x, cnugget.y)) {
-            executeOrder(unit, cnugget.x, cnugget.y, getBestTarget(
-                pieces, unit, cnugget.x, cnugget.y, TARGET_EVALUATOR));
-        
+            executeOrder(unit, cnugget.x, cnugget.y,
+                         getBestTarget(pieces, unit, cnugget.x, cnugget.y, TARGET_EVALUATOR));
+
         // if there's a loaded claim within reach, steal from it
         } else if (cclaim != null && containsAdjacent(moves, cclaim) &&
-            moveUnit(pieces, unit, moves, cclaim, 1)) {
-            return;
-        
+                   moveUnit(pieces, unit, moves, cclaim, 1)) {
+            // moveUnit did what we wanted
+
         // if there's a nugget holding target within reach, shoot it
         } else if (ctarget != null && attacks.contains(ctarget.x, ctarget.y)) {
             executeOrder(unit, Short.MAX_VALUE, 0, ctarget);
-        
+
         // otherwise, move towards nearest free nugget
         } else if (cnugget != null &&
             moveUnit(pieces, unit, moves, cnugget, 0)) {
-            return;
-        
+
         // or nearest loaded claim
-        } else if (cclaim != null &&
-            moveUnit(pieces, unit, moves, cclaim, 1)) {
-            return;
-            
+        } else if (cclaim != null && moveUnit(pieces, unit, moves, cclaim, 1)) {
+            // moveUnit did what we wanted
+
         // or nearest nugget holding target
-        } else if (ctarget != null &&
-            moveUnit(pieces, unit, moves, ctarget, -1)) {
-            return;
-        
+        } else if (ctarget != null && moveUnit(pieces, unit, moves, ctarget, -1)) {
+            // moveUnit did what we wanted
+
         // or nearest teleporter
-        } else if (tporter != null &&
-            moveUnit(pieces, unit, moves, tporter, 0)) {
-            return;
-            
-        // or just try to find something to shoot 
+        } else if (tporter != null && moveUnit(pieces, unit, moves, tporter, 0)) {
+            // moveUnit did what we wanted
+
+        // or just try to find something to shoot
         } else {
-            Piece target = getBestTarget(pieces, unit, attacks, 
-                    EMPTY_POINT_SET, TARGET_EVALUATOR);
+            Piece target = getBestTarget(pieces, unit, attacks, EMPTY_POINT_SET, TARGET_EVALUATOR);
             if (target != null) {
                 executeOrder(unit, Short.MAX_VALUE, 0, target);
             }
         }
     }
-    
+
     /**
-     * Attempts to move the unit towards the provided destination and fire
-     * off a shot at the best target.
+     * Attempts to move the unit towards the provided destination and fire off a shot at the best
+     * target.
      *
-     * @return true if we successfully moved towards the destination,
-     * false if we couldn't find a path
+     * @return true if we successfully moved towards the destination, false if we couldn't find a
+     * path
      */
-    protected boolean moveUnit (
-        Piece[] pieces, Unit unit, PointSet moves, Piece target, int tdist)
+    protected boolean moveUnit (Piece[] pieces, Unit unit, PointSet moves, Piece target, int tdist)
     {
-        return moveUnit(pieces, unit, moves, target.x, target.y, tdist,
-            TARGET_EVALUATOR);
+        return moveUnit(pieces, unit, moves, target.x, target.y, tdist, TARGET_EVALUATOR);
     }
-    
+
     /**
-     * Determines whether the point set contains any points adjacent to the
-     * given piece.
+     * Determines whether the point set contains any points adjacent to the given piece.
      */
     protected static boolean containsAdjacent (PointSet moves, Piece piece)
     {
@@ -183,36 +172,34 @@ public class GoldLogic extends AILogic
         }
         return false;
     }
-    
+
     /** Whether or not units can steal nuggets from claims. */
     protected boolean _stealing;
-    
+
     /** The location of our own claim. */
     protected Point _claimloc;
-    
-    /** Ranks units by properties that should make them good at gathering
-     * and stealing nuggets: speed and attack power. */
+
+    /** Ranks units by properties that should make them good at gathering and stealing nuggets:
+     * speed and attack power. */
     protected static final UnitConfigEvaluator OFFENSE_EVALUATOR =
         new UnitConfigEvaluator() {
         public int getWeight (UnitConfig config) {
             return config.moveDistance*10 + config.damage;
         }
     };
-    
-    /** Ranks potential targets by nugget holdingness, the amount of damage the
-     * unit will do, and the amount of damage the target has already taken. */
-    protected static final TargetEvaluator TARGET_EVALUATOR =
-        new TargetEvaluator() {
-        public int getWeight (BangObject bangobj, Unit unit, Piece target, 
-                int dist, PointSet preferredMoves) {
-            boolean nuggeted = (target instanceof Unit && 
-                    NuggetEffect.NUGGET_BONUS.equals(((Unit)target).holding)); 
-            return (nuggeted ?  1000 : 0) +
-                unit.computeScaledDamage(bangobj, target, 1f) * 
-                100 + target.damage;
+
+    /** Ranks potential targets by nugget holdingness, the amount of damage the unit will do, and
+     * the amount of damage the target has already taken. */
+    protected static final TargetEvaluator TARGET_EVALUATOR = new TargetEvaluator() {
+        public int getWeight (BangObject bangobj, Unit unit, Piece target, int dist,
+                              PointSet preferredMoves) {
+            boolean nuggeted = (target instanceof Unit &&
+                                NuggetEffect.NUGGET_BONUS.equals(((Unit)target).holding));
+            return (nuggeted ?  1000 : 0) + unit.computeScaledDamage(bangobj, target, 1f) * 100 +
+                target.damage;
         }
     };
-    
+
     /** When enemy units get this close to our (non-empty) claim, we start
      * sending units to defend it. */
     protected static final int DEFENSIVE_PERIMETER = 3;
