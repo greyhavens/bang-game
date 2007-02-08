ca5e0d3f44facbc8e9bf68fbf154b19a15b847ef
Michael Bayne <mdb@threerings.net>
2007-02-08 04:42:06 +0000

    Animate the display of end of game (or end of bounty rewards). More bling!

--- src/java/com/threerings/bang/game/client/AwardView.java
+++ src/java/com/threerings/bang/game/client/AwardView.java
@@ -10,6 +10,8 @@ import com.jmex.bui.BContainer;
 import com.jmex.bui.BImage;
 import com.jmex.bui.BLabel;
 import com.jmex.bui.Spacer;
+import com.jmex.bui.util.Dimension;
+import com.samskivert.util.Interval;
 import com.jmex.bui.icon.BlankIcon;
 import com.jmex.bui.icon.ImageIcon;
 import com.jmex.bui.layout.BorderLayout;
@@ -39,7 +41,7 @@ import com.threerings.bang.game.data.scenario.ScenarioInfo;
 public class AwardView extends BContainer
 {
     public AwardView (BasicContext ctx, BangObject bangobj, BangConfig bconfig, 
-                      PlayerObject user, Award award)
+                      final PlayerObject user, Award award)
     {
         super(GroupLayout.makeHoriz(GroupLayout.CENTER).
               setGap(25).setOffAxisPolicy(GroupLayout.STRETCH));
@@ -58,48 +60,47 @@ public class AwardView extends BContainer
                 msgs.get("m.endgame_coop_earnings") : msgs.get("m.endgame_earnings", rankstr));
         econt.add(new BLabel(txt, "endgame_title"), BorderLayout.NORTH);
 
-        BContainer rrow = new BContainer(new TableLayout(isBounty ? 3 : 7, 5, 5));
+        _table = new BContainer(new TableLayout(isBounty ? 3 : 7, 5, 5));
         // we need to center this verticaly
         BContainer vbox = GroupLayout.makeVBox(GroupLayout.CENTER);
-        vbox.add(rrow);
+        vbox.add(_table);
         econt.add(vbox, BorderLayout.CENTER);
 
         txt = isBounty ? msgs.get("m.bover_reward") : (isCoop ?
                 msgs.get("m.endgame_coop_reward") : msgs.get("m.endgame_reward", rankstr));
-        rrow.add(new BLabel(txt, "endgame_smallheader"));
+        _table.add(new BLabel(txt, "endgame_smallheader"));
 
         Purse purse;
         if (isBounty) {
-            rrow.add(new Spacer(70, 1));
+            _table.add(new Spacer(70, 1));
             purse = Purse.DEFAULT_PURSE;
         } else {
-            rrow.add(new Spacer(1, 1));
+            _table.add(new Spacer(1, 1));
             purse = user.getPurse();
             if (purse.getTownIndex() == 0) { // no purse
                 txt = msgs.get("m.endgame_nopurse");
             } else {
                 txt = ctx.xlate(BangCodes.GOODS_MSGS, purse.getName());
             }
-            rrow.add(new BLabel(txt, "endgame_smallheader"));
-            rrow.add(new Spacer(1, 1));
-            rrow.add(new BLabel(msgs.get("m.endgame_total"), "endgame_smallheader"));
-            rrow.add(new Spacer(30, 1));
+            _table.add(new BLabel(txt, "endgame_smallheader"));
+            _table.add(new Spacer(1, 1));
+            _table.add(new BLabel(msgs.get("m.endgame_total"), "endgame_smallheader"));
+            _table.add(new Spacer(30, 1));
         }
-        rrow.add(new BLabel(msgs.get("m.endgame_have"), "endgame_header"));
+        _table.add(new BLabel(msgs.get("m.endgame_have"), "endgame_header"));
 
-        NumberFormat cfmt = NumberFormat.getInstance();
         BLabel label;
-        txt = cfmt.format(Math.round(award.cashEarned / purse.getPurseBonus()));
-        rrow.add(label = new BLabel(txt, "endgame_smallcash"));
+        txt = _cfmt.format(Math.round(award.cashEarned / purse.getPurseBonus()));
+        _table.add(label = new BLabel(txt, "endgame_smallcash"));
         label.setIcon(BangUI.scripIcon);
         label.setIconTextGap(3);
 
         if (!isBounty) {
-            rrow.add(new BLabel("+", "endgame_smallcash"));
+            _table.add(new BLabel("+", "endgame_smallcash"));
 
             NumberFormat pfmt = NumberFormat.getPercentInstance();
             txt = pfmt.format(purse.getPurseBonus() - 1);
-            rrow.add(label = new BLabel(txt, "endgame_smallcash"));
+            _table.add(label = new BLabel(txt, "endgame_smallcash"));
             if (purse.getTownIndex() == 0) { // no purse
                 label.setIcon(new BlankIcon(64, 64));
             } else {
@@ -109,32 +110,41 @@ public class AwardView extends BContainer
                 label.setIcon(new ImageIcon(scaled));
             }
 
-            rrow.add(new BLabel("=", "endgame_smallcash"));
+            _table.add(new BLabel("=", "endgame_smallcash"));
 
-            txt = cfmt.format(award.cashEarned);
-            rrow.add(label = new BLabel(txt, "endgame_smallcash"));
+            txt = _cfmt.format(award.cashEarned);
+            _table.add(label = new BLabel(txt, "endgame_smallcash"));
             label.setIcon(BangUI.scripIcon);
             label.setIconTextGap(3);
         }
 
-        rrow.add(new Spacer(1, 1));
-        label = new BLabel(cfmt.format(user.scrip), "endgame_cash");
-        label.setIconTextGap(6);
-        label.setIcon(new ImageIcon(ctx.loadImage("ui/icons/big_scrip.png")));
-        rrow.add(label);
+        _table.add(new Spacer(1, 1));
+        final int scrip = user.scrip-award.cashEarned;
+        _scrip = new BLabel(_cfmt.format(scrip), "endgame_cash") {
+            protected Dimension computePreferredSize (int whint, int hhint) {
+                Dimension d = super.computePreferredSize(whint, hhint);
+                _maxwidth = d.width = Math.max(_maxwidth, d.width);
+                return d;
+            }
+            protected int _maxwidth;
+        };
+        _scrip.setIconTextGap(6);
+        _scrip.setIcon(new ImageIcon(ctx.loadImage("ui/icons/big_scrip.png")));
+        _table.add(_scrip);
 
         if (award.item instanceof Badge) {
-            BContainer bcont = new BContainer(new BorderLayout());
-            bcont.setStyleClass("endgame_border");
+            _badge = new BContainer(new BorderLayout());
+            _badge.setStyleClass("endgame_border");
             txt = msgs.get("m.endgame_badge");
-            bcont.add(new BLabel(txt, "endgame_title"), BorderLayout.NORTH);
-            bcont.add(new ItemIcon(ctx, award.item), BorderLayout.CENTER);
+            _badge.add(new BLabel(txt, "endgame_title"), BorderLayout.NORTH);
+            _badge.add(new ItemIcon(ctx, award.item), BorderLayout.CENTER);
             String reward = ((Badge)award.item).getReward();
             if (reward != null) {
                 txt = ctx.xlate(BangCodes.BADGE_MSGS, reward);
-                bcont.add(new BLabel(txt, "endgame_reward"), BorderLayout.SOUTH);
+                _badge.add(new BLabel(txt, "endgame_reward"), BorderLayout.SOUTH);
             }
-            add(bcont);
+            _badge.setAlpha(0f);
+            add(_badge);
 
         } else if (!bconfig.rated && !isBounty) {
             BContainer bcont = new BContainer(new BorderLayout());
@@ -144,5 +154,65 @@ public class AwardView extends BContainer
                       BorderLayout.CENTER);
             add(bcont);
         }
+
+        // start up an interval that will show their earnings
+        setVisible(0);
+        final int scripinc = (award.cashEarned > 200) ? 20 : 5;
+        final int cols = (_table.getComponentCount()/2)-1;
+        new Interval(ctx.getApp()) {
+            public void expired () {
+                if (_step < cols) {
+                    setVisible(++_step);
+                    setVisible(++_step);
+                    BangUI.play(BangUI.FeedbackSound.CHAT_RECEIVE);
+                    schedule(INTER_ANIM_DELAY);
+
+                } else if (_cscrip == 0) {
+                    _cscrip = scrip;
+                    setVisible(++_step);
+                    BangUI.play(BangUI.FeedbackSound.CHAT_RECEIVE);
+                    schedule(INTER_ANIM_DELAY);
+
+                } else if ((_cscrip + scripinc) < user.scrip) {
+                    BangUI.play(BangUI.FeedbackSound.KEY_TYPED);
+                    _cscrip += scripinc;
+                    _scrip.setText(_cfmt.format(_cscrip));
+                    schedule(SCRIP_ANIM_DELAY);
+
+                } else if (!_done) {
+                    _scrip.setText(_cfmt.format(user.scrip));
+                    BangUI.play(BangUI.FeedbackSound.ITEM_PURCHASE);
+                    _done = true;
+                    if (_badge != null) {
+                        schedule(POST_ANIM_DELAY);
+                    }
+
+                } else if (_badge != null) {
+                    _badge.setAlpha(1f);
+                    BangUI.play(BangUI.FeedbackSound.CHAT_SEND);
+                }
+            }
+
+            protected int _step, _cscrip;
+            protected boolean _done;
+        }.schedule(PRE_ANIM_DELAY);
     }
+
+    protected void setVisible (int columns)
+    {
+        for (int ii = 0, ll = _table.getComponentCount(); ii < ll; ii++) {
+            int column = ii%(ll/2);
+            _table.getComponent(ii).setAlpha(column < columns ? 1f : 0f);
+        }
+    }
+
+    protected BContainer _table;
+    protected BLabel _scrip;
+    protected BContainer _badge;
+    protected NumberFormat _cfmt = NumberFormat.getInstance();
+
+    protected static final long PRE_ANIM_DELAY = 1000L;
+    protected static final long INTER_ANIM_DELAY = 500L;
+    protected static final long SCRIP_ANIM_DELAY = 50L;
+    protected static final long POST_ANIM_DELAY = 2000L;
 }
