66d55ff713d67cf2c90068bd25d3b67deb1f1230
Michael Bayne <mdb@threerings.net>
2007-02-09 03:36:06 +0000

    Display any special rules for the bounty game (currently just no respawn).

--- src/java/com/threerings/bang/game/client/PreGameBountyView.java
+++ src/java/com/threerings/bang/game/client/PreGameBountyView.java
@@ -13,7 +13,10 @@ import com.jmex.bui.BLabel;
 import com.jmex.bui.event.ActionEvent;
 import com.jmex.bui.event.ActionListener;
 import com.jmex.bui.icon.ImageIcon;
+import com.jmex.bui.layout.BorderLayout;
 import com.jmex.bui.layout.GroupLayout;
+
+import com.threerings.util.MessageBundle;
 
 import com.threerings.bang.client.BangUI;
 import com.threerings.bang.client.bui.SteelWindow;
@@ -63,6 +66,7 @@ public class PreGameBountyView extends SteelWindow
         super(ctx, bounty.title + " - " + bounty.getGame(gameId).name);
         _ctrl = ctrl;
         setPreferredSize(770, -1);
+        MessageBundle msgs = ctx.getMessageManager().getBundle(GameCodes.GAME_MSGS);
 
         _contents.setStyleClass("bounty_pregame");
         _contents.setLayoutManager(GroupLayout.makeVert(GroupLayout.CENTER).setGap(25));
@@ -71,28 +75,38 @@ public class PreGameBountyView extends SteelWindow
         main.add(createSpeakerView(ctx, bounty, gameId, config,
                                    bounty.getGame(gameId).preGameQuote, false), GroupLayout.FIXED);
 
-        BContainer vert = new BContainer(
-            GroupLayout.makeVStretch().setOffAxisPolicy(GroupLayout.NONE));
-        vert.add(new BLabel(ctx.xlate(GameCodes.GAME_MSGS, "m.bounty_pregame"), "bounty_title"),
-                 GroupLayout.FIXED);
+        BContainer vert = new BContainer(new BorderLayout());
+        vert.add(new BLabel(msgs.get("m.bounty_pregame"), "bounty_title"), BorderLayout.NORTH);
 
-        BContainer ccont = new BContainer(GroupLayout.makeVert(GroupLayout.CENTER).
-                                          setOffAxisJustification(GroupLayout.LEFT));
+        BContainer ccont = new BContainer(
+            GroupLayout.makeVert(GroupLayout.CENTER).setOffAxisJustification(GroupLayout.LEFT));
         PlayerObject user = ctx.getUserObject();
         ImageIcon star = new ImageIcon(ctx.loadImage("ui/pregame/star.png"));
         for (Criterion crit : config.criteria) {
-            String msg = ctx.xlate(GameCodes.GAME_MSGS, crit.getDescription());
-            BLabel clabel = new BLabel(msg, "bounty_pregame_crit");
+            BLabel clabel = new BLabel(msgs.xlate(crit.getDescription()), "bounty_pregame_crit");
             clabel.setIcon(star);
             ccont.add(clabel);
         }
-        vert.add(ccont);
+        BContainer wrap = GroupLayout.makeHBox(GroupLayout.CENTER);
+        wrap.add(ccont);
+        vert.add(wrap, BorderLayout.CENTER);
+
+        // if there any special game conditions (currently just no respawn), display them
+        StringBuilder rules = new StringBuilder();
+        if (!config.respawnUnits) {
+            rules.append(msgs.get("m.no_respawn"));
+        }
+        if (rules.length() > 0) {
+            vert.add(new BLabel(msgs.get("m.bounty_rules", rules), "left_label"),
+                     BorderLayout.SOUTH);
+        }
+
         main.add(vert);
         _contents.add(main);
 
         _contents.add(new BLabel(bounty.getGame(gameId).preGameQuote.text, "bounty_quote"));
 
-        _buttons.add(new BButton(ctx.xlate(GameCodes.GAME_MSGS, "m.ready"), new ActionListener() {
+        _buttons.add(new BButton(msgs.get("m.ready"), new ActionListener() {
             public void actionPerformed (ActionEvent event) {
                 ctx.getBangClient().clearPopup(PreGameBountyView.this, true);
             }
