89a5b462e09ae60b7669442bfb3284886fb857b2
Mark Johnson <mjohnson@threerings.net>
2007-03-20 17:15:59 +0000

    Whoops, missed adding this file in my last massive check-in

---
+++ src/java/com/threerings/bang/data/GuestHandle.java
@@ -0,0 +1,37 @@
+//
+// $Id$
+
+package com.threerings.bang.data;
+
+/**
+ * A temporary handle for an unnamed player.
+ */
+public class GuestHandle extends Handle
+{
+    /** Set the default name for a guest handle. */
+    public static void setDefaultName (String name)
+    {
+        _defaultName = name;
+    }
+
+    /** Creates a blank instance of unerialization. */
+    public GuestHandle ()
+    {
+    }
+
+    /**
+     * Creates a guest handle instance with the supplied unique hash.
+     */
+    public GuestHandle (String hash)
+    {
+        super(hash);
+    }
+
+    @Override // documentation inherited
+    public String toString ()
+    {
+        return _defaultName;
+    }
+
+    protected static String _defaultName = "Guest Handle";
+}
