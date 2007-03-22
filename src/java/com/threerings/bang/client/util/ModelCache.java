799b024c075b864a785ba8d97945b6e034b50547
Andrzej Kapolka <andrzej@threerings.net>
2007-03-22 21:01:30 +0000

    Flush model VBOs and display lists when the prototypes are unloaded.

--- src/java/com/threerings/bang/client/util/ModelCache.java
+++ src/java/com/threerings/bang/client/util/ModelCache.java
@@ -3,25 +3,41 @@
 
 package com.threerings.bang.client.util;
 
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+
 import java.io.File;
 
 import java.nio.ByteOrder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import org.lwjgl.opengl.ARBBufferObject;
+import org.lwjgl.opengl.GL11;
 
 import com.jme.scene.Node;
 import com.jme.scene.Spatial;
+import com.jme.scene.VBOInfo;
+import com.jme.scene.batch.GeomBatch;
 import com.jme.scene.state.TextureState;
+import com.jme.util.geom.BufferUtils;
 
 import com.samskivert.util.ChainedResultListener;
+import com.samskivert.util.IntListUtil;
+import com.samskivert.util.Interval;
 import com.samskivert.util.Invoker;
 import com.samskivert.util.ListUtil;
 import com.samskivert.util.ObjectUtil;
 import com.samskivert.util.ResultHandler;
 import com.samskivert.util.ResultListener;
+import com.samskivert.util.SoftCache;
 
 import com.threerings.jme.model.Model;
 import com.threerings.jme.model.TextureProvider;
+import com.threerings.jme.util.BatchVisitor;
+
 import com.threerings.media.image.Colorization;
 
 import com.threerings.bang.client.BangPrefs;
@@ -42,6 +58,16 @@ public class ModelCache extends PrototypeCache<ModelCache.ModelKey, Model>
         // create a texture state here in order to make sure that the
         // texture state initialization isn't called from the loader
         _ctx.getRenderer().createTextureState();
+
+        // create the interval to flush cleared prototypes
+        new Interval(ctx.getApp()) {
+            public void expired () {
+                Reference<? extends ResultHandler<Model>> ref;
+                while ((ref = _cleared.poll()) != null) {
+                    PrototypeReference.class.cast(ref).flush();
+                }
+            }
+        }.schedule(FLUSH_INTERVAL, true);
     }
 
     /**
@@ -84,6 +110,17 @@ public class ModelCache extends PrototypeCache<ModelCache.ModelKey, Model>
         ResultListener<Model> rl)
     {
         getInstance(new ModelKey(type, name, variant), zations, rl);
+    }
+
+    @Override // documentation inherited
+    protected SoftCache<ModelKey, ResultHandler<Model>> createCache ()
+    {
+        return new SoftCache<ModelKey, ResultHandler<Model>>() {
+            protected SoftReference<ResultHandler<Model>> createReference (
+                ResultHandler<Model> handler) {
+                return new PrototypeReference(handler);
+            }
+        };
     }
 
     @Override // documentation inherited
@@ -144,6 +181,82 @@ public class ModelCache extends PrototypeCache<ModelCache.ModelKey, Model>
             instance.resolveTextures(new ModelTextureProvider(key, zations));
         }
         return instance;
+    }
+
+    /** Identifies the resources that must be released when the prototype is cleared. */
+    protected class PrototypeReference extends SoftReference<ResultHandler<Model>>
+    {
+        public PrototypeReference (ResultHandler<Model> handler)
+        {
+            super(handler, _cleared);
+            handler.getResult(new ResultListener<Model>() {
+                public void requestCompleted (Model result) {
+                    recordResources(result);
+                }
+                public void requestFailed (Exception cause) {
+                    // this will be reported elsewhere
+                }
+            });
+        }
+
+        /**
+         * Deletes the resources held by the prototype.
+         */
+        public void flush ()
+        {
+            if (_lists != null) {
+                for (int list : _lists) {
+                    GL11.glDeleteLists(list, 1);
+                }
+                _lists = null;
+            }
+            if (_vbois == null) {
+                return;
+            }
+
+            int[] buffers = null;
+            for (VBOInfo vboi : _vbois) {
+                buffers = maybeAdd(buffers, vboi.getVBOVertexID());
+                buffers = maybeAdd(buffers, vboi.getVBONormalID());
+                buffers = maybeAdd(buffers, vboi.getVBOIndexID());
+                buffers = maybeAdd(buffers, vboi.getVBOColorID());
+                for (int ii = 0, nn = TextureState.getNumberOfTotalUnits(); ii < nn; ii++) {
+                    buffers = maybeAdd(buffers, vboi.getVBOTextureID(ii));
+                }
+            }
+            if (buffers != null) {
+                ARBBufferObject.glDeleteBuffersARB(BufferUtils.createIntBuffer(buffers));
+            }
+            _vbois = null;
+        }
+
+        protected void recordResources (Model prototype)
+        {
+            final ArrayList<VBOInfo> vbois = new ArrayList<VBOInfo>();
+            new BatchVisitor() {
+                protected void visit (GeomBatch batch) {
+                    VBOInfo vboi = batch.getVBOInfo();
+                    if (vboi != null) {
+                        vbois.add(vboi);
+                    }
+                    _lists = maybeAdd(_lists, batch.getDisplayListID());
+                }
+            }.traverse(prototype);
+
+            _vbois = vbois.isEmpty() ? null : vbois.toArray(new VBOInfo[vbois.size()]);
+            _lists = (_lists == null) ? null : IntListUtil.compact(_lists);
+        }
+
+        protected int[] maybeAdd (int[] values, int value)
+        {
+            return (value > 0) ? IntListUtil.add(values, value) : values;
+        }
+
+        /** The VBOs to delete. */
+        protected VBOInfo[] _vbois;
+
+        /** The display lists to delete. */
+        protected int[] _lists;
     }
 
     /** Resolved model textures using the texture cache. */
@@ -223,4 +336,11 @@ public class ModelCache extends PrototypeCache<ModelCache.ModelKey, Model>
             this.type = type;
         }
     }
+
+    /** The queue of prototypes to destroy. */
+    protected ReferenceQueue<ResultHandler<Model>> _cleared =
+        new ReferenceQueue<ResultHandler<Model>>();
+
+    /** The rate at which to check for cleared prototypes to destroy. */
+    protected static final long FLUSH_INTERVAL = 5000L;
 }
--- src/java/com/threerings/bang/client/util/PrototypeCache.java
+++ src/java/com/threerings/bang/client/util/PrototypeCache.java
@@ -24,28 +24,36 @@ public abstract class PrototypeCache<S, T>
     public PrototypeCache (BasicContext ctx)
     {
         _ctx = ctx;
+        _prototypes = createCache();
     }
 
     /**
-     * Creates an instance of the identified object, loading its prototype
-     * on the invoker thread if necessary.
+     * Creates the cache object to store the prototype handlers.
+     */
+    protected SoftCache<S, ResultHandler<T>> createCache ()
+    {
+        return new SoftCache<S, ResultHandler<T>>();
+    }
+
+    /**
+     * Creates an instance of the identified object, loading its prototype on the invoker thread if
+     * necessary.
      *
-     * @param rl the listener to receive the instance, or <code>null</code>
-     * to preload the prototype.
+     * @param rl the listener to receive the instance, or <code>null</code> to preload the
+     * prototype.
      */
     protected void getInstance (
         S key, Colorization[] zations, ResultListener<T> rl)
     {
-        getPrototype(key, (rl == null) ?
-            null : new InstanceCreator(key, zations, rl));
+        getPrototype(key, (rl == null) ? null : new InstanceCreator(key, zations, rl));
     }
 
     /**
-     * Fetches the prototype for the identified object, loading it on the
-     * invoker thread if necessary.
+     * Fetches the prototype for the identified object, loading it on the invoker thread if
+     * necessary.
      *
-     * @param rl the listener to receive the instance, or <code>null</code>
-     * to preload the prototype.
+     * @param rl the listener to receive the instance, or <code>null</code> to preload the
+     * prototype.
      */
     protected void getPrototype (S key, ResultListener<T> rl)
     {
@@ -89,8 +97,7 @@ public abstract class PrototypeCache<S, T>
     }
 
     /**
-     * Loads the prototype corresponding to the given key on the invoker
-     * thread.
+     * Loads the prototype corresponding to the given key on the invoker thread.
      */
     protected abstract T loadPrototype (S key)
         throws Exception;
@@ -103,8 +110,7 @@ public abstract class PrototypeCache<S, T>
     /**
      * Creates and returns a new instance from the given prototype.
      */
-    protected abstract T createInstance (
-        S key, T prototype, Colorization[] zations);
+    protected abstract T createInstance (S key, T prototype, Colorization[] zations);
 
     /**
      * Normalizes the provided relative path.
@@ -119,8 +125,8 @@ public abstract class PrototypeCache<S, T>
         return npath;
     }
 
-    /** Listens for the loading of a prototype in order to generate an
-     * instance based on that prototype. */
+    /** Listens for the loading of a prototype in order to generate an instance based on that
+     * prototype. */
     protected class InstanceCreator
         implements ResultListener<T>
     {
@@ -147,8 +153,7 @@ public abstract class PrototypeCache<S, T>
         /** The object key. */
         protected S _key;
 
-        /** The colorizations to apply to the instance, or <code>null</code>
-         * for none. */
+        /** The colorizations to apply to the instance, or <code>null</code> for none. */
         protected Colorization[] _zations;
 
         /** The listener waiting for the instance. */
@@ -159,7 +164,7 @@ public abstract class PrototypeCache<S, T>
     protected BasicContext _ctx;
 
     /** Maps keys to prototype handlers. */
-    protected SoftCache<S, ResultHandler<T>> _prototypes = new SoftCache<S, ResultHandler<T>>();
+    protected SoftCache<S, ResultHandler<T>> _prototypes;
 
     /** Used to normalize relative paths. */
     protected static final String PATH_DOTDOT = "[^/.]+/\\.\\./";
