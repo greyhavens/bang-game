225e88a6549c482ac24fce0a5a0f07b49e95795b
Mark Johnson <mjohnson@threerings.net>
2007-05-17 18:54:52 +0000

    Better include the warning notification

---
+++ src/java/com/threerings/bang/data/Warning.java
@@ -0,0 +1,73 @@
+//
+// $Id$
+
+package com.threerings.bang.data;
+
+import com.threerings.util.MessageBundle;
+
+/**
+ * Represents a warning to a player.
+ */
+public class Warning extends Notification
+{
+    /** A warning type. */
+    public static final String TEMP_BAN = "m.warning_tb";
+
+    /** A warning type. */
+    public static final String WARNING = "m.warning";
+
+    /** The type of warning. */
+    public String type;
+
+    /** The message associated to this warning. */
+    public String message;
+
+    /**
+     * Creates a new warning message on the server.
+     */
+    public Warning (String type, String message, ResponseHandler handler)
+    {
+        super(handler);
+        this.type = type;
+        this.message = message;
+    }
+
+    /**
+     * No-arg constructor for deserialization.
+     */
+    public Warning ()
+    {
+    }
+
+    // documentation inherited from interface DSet.Entry
+    public Comparable getKey ()
+    {
+        return message;
+    }
+
+    @Override // documentation inherited
+    public String getTitle ()
+    {
+        return type + "_title";
+    }
+
+    @Override // documentation inherited
+    public String getText ()
+    {
+        return MessageBundle.tcompose(type + "_info", message);
+    }
+
+    @Override // documentation inherited
+    public String [] getResponses ()
+    {
+        return new String[] { "m.i_understand" };
+    }
+
+    @Override // documentation inherited
+    public int getEnabledDelay ()
+    {
+        return BUTTON_DELAY;
+    }
+
+    protected static final int BUTTON_DELAY = 30;
+}
