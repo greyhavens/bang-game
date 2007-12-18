636bff34fed162ef99d27906b1eeb256138ff02a
Michael Bayne <mdb@threerings.net>
2007-12-18 22:44:18 +0000

    Bye bye JME, you live in ooo-libs now.

--- projects/jme/src/com/jmex/awt/swingui/dnd/JMEDragGestureListener.java
+++
@@ -1,9 +0,0 @@
-package com.jmex.awt.swingui.dnd;
-
-/**
- * @author Galun
- */
-public interface JMEDragGestureListener {
-
-    public void dragGestureRecognized( JMEDragGestureEvent dge );
-}
--- projects/jme/src/com/jmex/font3d/math/GeometricException.java
+++
@@ -1,9 +0,0 @@
-package com.jmex.font3d.math;
-
-public class GeometricException extends RuntimeException
-{
-	public GeometricException(String str)
-	{
-		super(str);
-	}
-}
